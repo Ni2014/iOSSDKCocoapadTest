ef78d647421aec57749a9f22ed115a20e8e6430b
宸笙 <1806559096@qq.com>
2019-09-10 11:05:28 +0800

    init framework

---
+++ Frameworks/FuncSDK.framework/Headers/FuncSDK.h
@@ -0,0 +1,21 @@
+//
+//  FuncSDK.h
+//  FuncSDK
+//
+//  Created by nothing on 2019/9/10.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Test.h"
+
+//! Project version number for FuncSDK.
+FOUNDATION_EXPORT double FuncSDKVersionNumber;
+
+//! Project version string for FuncSDK.
+FOUNDATION_EXPORT const unsigned char FuncSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FuncSDK/PublicHeader.h>
+
+
---
+++ Frameworks/FuncSDK.framework/Headers/Test.h
@@ -0,0 +1,19 @@
+//
+//  Test.h
+//  FuncSDK
+//
+//  Created by nothing on 2019/9/10.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+
++(void)test;
+@end
+
+NS_ASSUME_NONNULL_END
